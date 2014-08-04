52b2aafa45a823b7635e3706f447ed21e0da0497
Graham Helliwell <graham.helliwell@red-gate.com>
2014-08-04 13:41:51 +0100

    Start command line project

---
+++ ParseArguments/ParseArguments/Tests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using NUnit.Framework;
+using System.Linq;
+
+namespace ParseArguments
+{
+    [TestFixture]
+    public class Tests
+    {
+        [Test]
+        public void GivenZeroArgsReturnsZeroParsedArgs()
+        {
+            var parsedArgs = ParseArgs(new String[0]);
+            Assert.That(parsedArgs.Count, Is.EqualTo(0));
+        }
+
+        private IDictionary<char, object> ParseArgs(string[] args)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
